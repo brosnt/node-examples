96eae011ccfbb5c43f568a86418801a48c99b6bc
brosnt <trevor@plastiq.com>
2015-09-11 23:15:43 -0700

    Updated license, corrected display bug in nools example

--- nools/server.js
+++ nools/server.js
@@ -27,9 +27,9 @@ session.match().then(
 	  //promise that is resolved once there are no more rules to execute
 	  console.log("Completed matching of facts against our ruleset");
 	  console.log("p1 Amount=["+ p1.amount +"], HeldForReview=["+ p1.holdForManualReview +"], Rejected=["+ p1.rejected +"]");
-	  console.log("p2 Amount=["+ p1.amount +"], HeldForReview=["+ p2.holdForManualReview +"], Rejected=["+ p2.rejected +"]");
-	  console.log("p3 Amount=["+ p1.amount +"], HeldForReview=["+ p3.holdForManualReview +"], Rejected=["+ p3.rejected +"]");
-	  console.log("p4 Amount=["+ p1.amount +"], HeldForReview=["+ p4.holdForManualReview +"], Rejected=["+ p4.rejected +"]");
+	  console.log("p2 Amount=["+ p2.amount +"], HeldForReview=["+ p2.holdForManualReview +"], Rejected=["+ p2.rejected +"]");
+	  console.log("p3 Amount=["+ p3.amount +"], HeldForReview=["+ p3.holdForManualReview +"], Rejected=["+ p3.rejected +"]");
+	  console.log("p4 Amount=["+ p4.amount +"], HeldForReview=["+ p4.holdForManualReview +"], Rejected=["+ p4.rejected +"]");
 
 	  //cleanup - purge the current session of all facts (releases memory)
 	  session.dispose();
